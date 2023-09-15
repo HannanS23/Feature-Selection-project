606d46142dd1487e7f32d6ace6d8fd0db7215180
HannanS23 <144708343+HannanS23@users.noreply.github.com>
2023-09-15 12:02:32 +0100

    Update and rename Lasso0.075.py to Ridge.py

--- Ridge.py
+++ Ridge.py
@@ -18,40 +18,48 @@ file_path = 'C:/Users/shaha/OneDrive/MSc Data Science & Business Analytics/PROJ5
 # Read the Excel file into a DataFrame
 data = pd.read_excel(file_path)
 
-
 X = data.drop("Grade", axis=1)  # Features
 y = data["Grade"]  # Target variable
 
-# Standardize the features (important for regularization methods like Lasso)
+# Standardize the features (important for regularization methods)
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
+ 
+# Create a Ridge regression model
+ridge_model = Ridge(alpha=1.0)  # Adjust the value of alpha
 
-# Perform Lasso feature selection
-alpha = 0.01 # Regularization strength (adjust as needed)
-lasso_model = Lasso(alpha=alpha)
-lasso_model.fit(X_scaled, y)
+# Perform feature selection using Ridge regression
+selector = SelectFromModel(ridge_model)
+X_selected = selector.fit_transform(X, y)
 
-# Get the coefficients of the features
-feature_coeffs = lasso_model.coef_
-
-# Get the selected feature names (where coefficient is not zero)
-selected_feature_names = X.columns[feature_coeffs != 0]
+# Get the selected feature names
+selected_feature_names = X.columns[selector.get_support()]
 
 # Print the selected feature names
-print(selected_feature_names)
+#print("Selected Features:")
+#print(selected_feature_names)
 
 # Create a DataFrame with the selected features
 X_selected = X[selected_feature_names]
 
 # Create an SVM classifier
 svm_classifier = SVC(kernel='linear', C=1.0)
-
+\
 # Perform cross-validation and get predicted labels for each fold
 # 'cv' specifies the number of folds for cross-validation (e.g., cv=5 for 5-fold cross-validation)
-cv_predictions = cross_val_predict(svm_classifier, X_selected, y, cv=5)
+cv_predictions = cross_val_predict(svm_classifier, X_selected, y, cv=3)
 
 # Calculate evaluation metrics for the cross-validated predictions
 accuracy = accuracy_score(y, cv_predictions)
 precision = precision_score(y, cv_predictions)
 recall = recall_score(y, cv_predictions)
 f1 = f1_score(y, cv_predictions)
+
+# Print the results
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall (Sensitivity):", recall)
+print("Specificity:", specificity)
+print("F1 Score:", f1)
+print("AUC-ROC Score:", auc_roc)
+
