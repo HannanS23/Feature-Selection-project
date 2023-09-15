bfdad4ce82689b91418282aa359d24ec948af350
HannanS23 <144708343+HannanS23@users.noreply.github.com>
2023-09-15 12:07:53 +0100

    Update Ridge.py

--- Ridge.py
+++ Ridge.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_predict
